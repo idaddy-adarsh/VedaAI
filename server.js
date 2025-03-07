2b9749de0272d1cfbe855d9e08527439b5831078
idaddy-adarsh <unknownplayers001@outlook.com>
2025-03-07 16:28:48 +0530

    Initial commit:hhh

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "process.env.CALLBACK_URL"
 },
 async function(accessToken, refreshToken, profile, done) {
     const users = readUserData();
